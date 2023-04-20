400f19962c0506b54147e47779fcd054ebf52328
Ben Christel <ben.christel@gmail.com>
2023-04-19 20:17:00 -0700

    Split files.impl.ts out of files.ts

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,49 +1,15 @@
+export { listDeep, writeDeep } from "./files.impl";
+import { listDeep, writeDeep } from "./files.impl";
 import { TmpDir } from "../testing/tmp-dir";
 import { test, expect, equals, is } from "@benchristel/taste";
-import { join, relative, dirname } from "path";
-import * as fs from "fs/promises";
 import { valuesToStrings } from "./objects";
 import { buffer } from "./buffer";
 
 export type FileSet = Record<string, Buffer>;
 
-export async function listDeep(
-  dir: string,
-  root: string = dir
-): Promise<FileSet> {
-  let fileSet: FileSet = {};
-  const entries = await fs.readdir(dir, { withFileTypes: true });
-
-  const promises: Array<Promise<unknown>> = entries.map((entry) => {
-    const path = join(dir, entry.name);
-    const pathFromRoot = "/" + relative(root, path);
-
-    if (entry.isFile()) {
-      return fs.readFile(path).then((contents) => {
-        fileSet[pathFromRoot] = contents;
-      });
-    }
-
-    if (entry.isDirectory()) {
-      return listDeep(path, root).then((listing) => {
-        fileSet = { ...fileSet, ...listing };
-      });
-    }
-
-    return Promise.resolve();
-  });
-
-  await Promise.all(promises);
-  return fileSet;
-}
-
-export async function writeDeep(dir: string, fileSet: FileSet): Promise<void> {
-  const promises = Object.entries(fileSet).map(([path, contents]) => {
-    return fs
-      .mkdir(join(dir, dirname(path)), { recursive: true })
-      .then(() => fs.writeFile(join(dir, path), contents));
-  });
-  await Promise.all(promises);
+{
+  listDeep satisfies (path: string) => Promise<FileSet>;
+  writeDeep satisfies (path: string, files: FileSet) => Promise<void>;
 }
 
 test("listDeep", {
