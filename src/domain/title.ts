a3ff073e68f0c047fb5f9b007359f7919d1df9ac
Ben Christel <ben.christel@gmail.com>
2023-04-27 18:28:55 -0700

    Parse titles that have sub-elements

--- src/domain/title.ts
+++ src/domain/title.ts
@@ -1,5 +1,35 @@
 import { basename } from "path";
+import * as cheerio from "cheerio";
+import { text } from "cheerio";
+import { test, expect, is } from "@benchristel/taste";
 
 export function title(path: string, html: string): string {
-  return html.match(/<h1[^>]*>([^<]+)<\/h1>/)?.[1] ?? basename(path);
+  const $ = cheerio.load(html);
+  return text($("h1").slice(0, 1)) || basename(path);
 }
+
+test("title", {
+  "extracts the title from an h1 tag"() {
+    expect(title("", "<h1>foo</h1>"), is, "foo");
+  },
+
+  "extracts the title from an h1 tag with an id"() {
+    expect(title("", `<h1 id="the-id">foo</h1>`), is, "foo");
+  },
+
+  "extracts the title from an h1 tag with child tags"() {
+    expect(title("", `<h1><code>foo</code></h1>`), is, "foo");
+  },
+
+  "uses the title from the first h1 tag if there are several"() {
+    expect(title("", `<h1>the title</h1><h1>not this</h1>`), is, "the title");
+  },
+
+  "defaults to the filename if there is no h1"() {
+    expect(title("dir/file.html", `<p>whoa</p>`), is, "file.html");
+  },
+
+  "defaults to the filename if the h1 is empty"() {
+    expect(title("file.html", `<h1></h1>`), is, "file.html");
+  },
+});
