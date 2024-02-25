cd139af05310eb4114ec3a4b57cc2b6eeb5b51b1
Ben Christel <ben.christel@gmail.com>
2024-02-25 11:14:07 -0800

    Move markdown tests to their own file

--- dist/lib/markdown.js
+++ dist/lib/markdown.js
@@ -1,28 +1,10 @@
 import { marked } from "marked";
-import { test, expect, is } from "@benchristel/taste";
 import { markedHighlight } from "marked-highlight";
 import hljs from "highlight.js";
 import { macroPattern } from "../domain/macros/parser.js";
 export function htmlFromMarkdown(md) {
     return marked.parse(md);
 }
-test("htmlFromMarkdown", {
-    "renders an empty document"() {
-        expect(htmlFromMarkdown(""), is, "");
-    },
-    "renders markdown to HTML"() {
-        expect(htmlFromMarkdown("# Hello"), is, `<h1 id="hello">Hello</h1>\n`);
-    },
-    "converts wiki-style links to HTML links"() {
-        expect(htmlFromMarkdown("[[MyWikiPage]]"), is, `<p><a href="MyWikiPage.html">MyWikiPage</a></p>\n`);
-    },
-    "ignores wiki-style links with an alias"() {
-        // Obsidian and GitHub Pages have opposite conventions for which half is
-        // the filename and which half is the alias, so we can't do anything
-        // sensible with these links.
-        expect(htmlFromMarkdown("[[Foo|Bar]]"), is, `<p>[[Foo|Bar]]</p>\n`);
-    },
-});
 const wikiLink = {
     name: "wikiLink",
     level: "inline",
---
+++ src/lib/markdown.test.ts
@@ -0,0 +1,27 @@
+import { test, expect, is } from "@benchristel/taste";
+import { htmlFromMarkdown } from "./markdown";
+
+test("htmlFromMarkdown", {
+  "renders an empty document"() {
+    expect(htmlFromMarkdown(""), is, "");
+  },
+
+  "renders markdown to HTML"() {
+    expect(htmlFromMarkdown("# Hello"), is, `<h1 id="hello">Hello</h1>\n`);
+  },
+
+  "converts wiki-style links to HTML links"() {
+    expect(
+      htmlFromMarkdown("[[MyWikiPage]]"),
+      is,
+      `<p><a href="MyWikiPage.html">MyWikiPage</a></p>\n`
+    );
+  },
+
+  "ignores wiki-style links with an alias"() {
+    // Obsidian and GitHub Pages have opposite conventions for which half is
+    // the filename and which half is the alias, so we can't do anything
+    // sensible with these links.
+    expect(htmlFromMarkdown("[[Foo|Bar]]"), is, `<p>[[Foo|Bar]]</p>\n`);
+  },
+});
