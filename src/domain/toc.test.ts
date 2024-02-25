f15570571037a4c405f927b94d4bef520b0d6978
Ben Christel <ben.christel@gmail.com>
2024-02-25 08:41:29 -0800

    Move tests for TOC to their own file

---
+++ src/domain/toc.test.ts
@@ -0,0 +1,167 @@
+import { test, expect, is, equals } from "@benchristel/taste";
+import { toc, htmlToc, leaf, branch } from "./toc.js";
+
+test("toc", {
+  "given an empty set of files"() {
+    expect(toc([]), equals, []);
+  },
+
+  "excludes the root index.html file"() {
+    const files = [{ path: "/index.html", title: "whatever" }];
+    expect(toc(files), equals, []);
+  },
+
+  "excludes the index.html under the given root"() {
+    const files = [{ path: "/foo/index.html", title: "whatever" }];
+    expect(toc(files, "/foo"), equals, []);
+  },
+
+  "given several files"() {
+    const files = [
+      { path: "/aaa.html", title: "aaa.html" },
+      { path: "/bbb.html", title: "bbb.html" },
+    ];
+    const expected = [
+      leaf({ path: "/aaa.html", title: "aaa.html" }),
+      leaf({ path: "/bbb.html", title: "bbb.html" }),
+    ];
+    expect(toc(files), equals, expected);
+  },
+
+  "given an index.html file in a subdirectory"() {
+    const files = [{ path: "/sub/index.html", title: "sub" }];
+    const expected = [branch({ path: "/sub/index.html", title: "sub" })];
+    expect(toc(files), equals, expected);
+  },
+
+  "given a subdirectory with several files"() {
+    const files = [
+      { path: "/sub/index.html", title: "sub" },
+      { path: "/sub/aaa.html", title: "aaa.html" },
+      { path: "/sub/bbb.html", title: "bbb.html" },
+    ];
+    const expected = [
+      branch(
+        { path: "/sub/index.html", title: "sub" },
+        leaf({ path: "/sub/aaa.html", title: "aaa.html" }),
+        leaf({ path: "/sub/bbb.html", title: "bbb.html" })
+      ),
+    ];
+    expect(toc(files), equals, expected);
+  },
+
+  "given a sub-subdirectory"() {
+    const files = [
+      { path: "/sub/index.html", title: "sub" },
+      { path: "/sub/marine/index.html", title: "marine" },
+      { path: "/sub/marine/aaa.html", title: "aaa.html" },
+      { path: "/sub/marine/bbb.html", title: "bbb.html" },
+      { path: "/sub/marine/ccc.html", title: "ccc.html" },
+    ];
+    const expected = [
+      branch(
+        { path: "/sub/index.html", title: "sub" },
+        branch(
+          { path: "/sub/marine/index.html", title: "marine" },
+          leaf({ path: "/sub/marine/aaa.html", title: "aaa.html" }),
+          leaf({ path: "/sub/marine/bbb.html", title: "bbb.html" }),
+          leaf({ path: "/sub/marine/ccc.html", title: "ccc.html" })
+        )
+      ),
+    ];
+    expect(toc(files), equals, expected);
+  },
+
+  "keeps files in order"() {
+    const files = [
+      { path: "/bbb.html", title: "1" },
+      { path: "/ddd.html", title: "2" },
+      { path: "/aaa.html", title: "3" },
+      { path: "/ccc.html", title: "4" },
+    ];
+    const expected = [
+      leaf({ path: "/bbb.html", title: "1" }),
+      leaf({ path: "/ddd.html", title: "2" }),
+      leaf({ path: "/aaa.html", title: "3" }),
+      leaf({ path: "/ccc.html", title: "4" }),
+    ];
+    expect(toc(files), equals, expected);
+  },
+
+  "keeps directories in order"() {
+    const files = [
+      { path: "/index.html", title: "Homepage" },
+      { path: "/aaa/index.html", title: "aaa" },
+      { path: "/aaa/foo.html", title: "foo.html" },
+      { path: "/bbb/index.html", title: "bbb" },
+      { path: "/bbb/foo.html", title: "foo.html" },
+      { path: "/ccc/index.html", title: "ccc" },
+      { path: "/ccc/foo.html", title: "foo.html" },
+      { path: "/ddd/index.html", title: "ddd" },
+      { path: "/ddd/foo.html", title: "foo.html" },
+    ];
+    const expected = [
+      branch(
+        { path: "/aaa/index.html", title: "aaa" },
+        leaf({ path: "/aaa/foo.html", title: "foo.html" })
+      ),
+      branch(
+        { path: "/bbb/index.html", title: "bbb" },
+        leaf({ path: "/bbb/foo.html", title: "foo.html" })
+      ),
+      branch(
+        { path: "/ccc/index.html", title: "ccc" },
+        leaf({ path: "/ccc/foo.html", title: "foo.html" })
+      ),
+      branch(
+        { path: "/ddd/index.html", title: "ddd" },
+        leaf({ path: "/ddd/foo.html", title: "foo.html" })
+      ),
+    ];
+    expect(toc(files), equals, expected);
+  },
+});
+
+test("htmlToc", {
+  "given an empty set of files"() {
+    expect(htmlToc([], "/index.html"), is, "");
+  },
+
+  "given a tree with one file"() {
+    const files = [{ path: "/foo.html", title: "This Is Foo" }];
+
+    const expected = `<ul><li><a href="foo.html">This Is Foo</a></li></ul>`;
+
+    expect(htmlToc(files, "/index.html"), is, expected);
+  },
+
+  "generates a list of multiple links"() {
+    const files = [
+      { path: "/bar.html", title: "Bar" },
+      { path: "/foo.html", title: "Foo" },
+    ];
+
+    const expected = `<ul><li><a href="bar.html">Bar</a></li><li><a href="foo.html">Foo</a></li></ul>`;
+
+    expect(htmlToc(files, "/index.html"), is, expected);
+  },
+
+  "creates relative links, starting from the linkOrigin"() {
+    const files = [{ path: "/foo.html", title: "Foo" }];
+
+    const expected = `<ul><li><a href="../../../foo.html">Foo</a></li></ul>`;
+
+    expect(htmlToc(files, "/one/two/three/foo.html", "/"), is, expected);
+  },
+
+  recurses() {
+    const files = [
+      { path: "/bar/index.html", title: "Bar" },
+      { path: "/bar/baz.html", title: "Baz" },
+    ];
+
+    const expected = `<ul><li><a href="bar/index.html">Bar</a><ul><li><a href="bar/baz.html">Baz</a></li></ul></li></ul>`;
+
+    expect(htmlToc(files, "/index.html"), is, expected);
+  },
+});
--- src/domain/toc.ts
+++ src/domain/toc.ts
@@ -1,184 +1,86 @@
-export { htmlToc } from "./toc.impl.js";
-import { test, expect, is, equals } from "@benchristel/taste";
-import { toc, htmlToc, leaf, branch } from "./toc.impl.js";
+import { contains, removePrefix, removeSuffix } from "../lib/strings.js";
+import { dirname, relative } from "path";
+import { ensureTrailingSlash } from "../lib/paths.js";
 import { Linkable } from "./project-global-info.js";
 
-{
-  // htmlToc() generates an HTML "tree of contents" with <ul> and <li>
-  // elements, and links.
-  htmlToc satisfies (
-    orderedLinkables: Linkable[],
-    // linkOrigin: a directory path. Paths in link hrefs will be relative to
-    // linkOrigin. Links are always generated with relative paths, so sites
-    // can be deployed to a subdirectory of a domain.
-    linkOrigin: string,
-    // root: a directory, which defaults to linkOrigin. The generated tree of
-    // contents will include only files under the given root.
-    root: string | undefined
-  ) => string;
+export type TreeOfContents = Array<Node>;
+
+export type Node = Branch | Leaf;
+
+export type Branch = {
+  type: "branch";
+  path: string;
+  title: string;
+  contents: TreeOfContents;
+};
+
+export type Leaf = {
+  type: "leaf";
+  path: string;
+  title: string;
+};
+
+export function branch(
+  params: { path: string; title: string },
+  ...contents: TreeOfContents
+): Branch {
+  return { type: "branch", contents, ...params };
 }
 
-test("toc", {
-  "given an empty set of files"() {
-    expect(toc([]), equals, []);
-  },
+export function leaf(params: { path: string; title: string }): Leaf {
+  return { type: "leaf", ...params };
+}
 
-  "excludes the root index.html file"() {
-    const files = [{ path: "/index.html", title: "whatever" }];
-    expect(toc(files), equals, []);
-  },
+export function toc(
+  orderedLinkables: Linkable[],
+  root: string = "/"
+): TreeOfContents {
+  root = ensureTrailingSlash(root);
+  return Object.values(orderedLinkables)
+    .filter(
+      ({ path }) =>
+        path.startsWith(root) &&
+        path.endsWith(".html") &&
+        path !== root + "index.html" &&
+        !contains("/", removeSuffix(removePrefix(path, root), "/index.html"))
+    )
+    .map(
+      ({ path, title }): Node =>
+        path.endsWith("/index.html")
+          ? branch(
+              { path, title },
+              ...toc(orderedLinkables, removeSuffix(path, "index.html"))
+            )
+          : leaf({ path, title })
+    );
+}
 
-  "excludes the index.html under the given root"() {
-    const files = [{ path: "/foo/index.html", title: "whatever" }];
-    expect(toc(files, "/foo"), equals, []);
-  },
+export function htmlToc(
+  orderedLinkables: Linkable[],
+  linkOrigin: string,
+  root: string = dirname(linkOrigin)
+): string {
+  const theToc = toc(orderedLinkables, root);
+  if (theToc.length === 0) {
+    return "";
+  }
 
-  "given several files"() {
-    const files = [
-      { path: "/aaa.html", title: "aaa.html" },
-      { path: "/bbb.html", title: "bbb.html" },
-    ];
-    const expected = [
-      leaf({ path: "/aaa.html", title: "aaa.html" }),
-      leaf({ path: "/bbb.html", title: "bbb.html" }),
-    ];
-    expect(toc(files), equals, expected);
-  },
+  return htmlForToc(theToc, linkOrigin);
+}
 
-  "given an index.html file in a subdirectory"() {
-    const files = [{ path: "/sub/index.html", title: "sub" }];
-    const expected = [branch({ path: "/sub/index.html", title: "sub" })];
-    expect(toc(files), equals, expected);
-  },
-
-  "given a subdirectory with several files"() {
-    const files = [
-      { path: "/sub/index.html", title: "sub" },
-      { path: "/sub/aaa.html", title: "aaa.html" },
-      { path: "/sub/bbb.html", title: "bbb.html" },
-    ];
-    const expected = [
-      branch(
-        { path: "/sub/index.html", title: "sub" },
-        leaf({ path: "/sub/aaa.html", title: "aaa.html" }),
-        leaf({ path: "/sub/bbb.html", title: "bbb.html" })
-      ),
-    ];
-    expect(toc(files), equals, expected);
-  },
-
-  "given a sub-subdirectory"() {
-    const files = [
-      { path: "/sub/index.html", title: "sub" },
-      { path: "/sub/marine/index.html", title: "marine" },
-      { path: "/sub/marine/aaa.html", title: "aaa.html" },
-      { path: "/sub/marine/bbb.html", title: "bbb.html" },
-      { path: "/sub/marine/ccc.html", title: "ccc.html" },
-    ];
-    const expected = [
-      branch(
-        { path: "/sub/index.html", title: "sub" },
-        branch(
-          { path: "/sub/marine/index.html", title: "marine" },
-          leaf({ path: "/sub/marine/aaa.html", title: "aaa.html" }),
-          leaf({ path: "/sub/marine/bbb.html", title: "bbb.html" }),
-          leaf({ path: "/sub/marine/ccc.html", title: "ccc.html" })
-        )
-      ),
-    ];
-    expect(toc(files), equals, expected);
-  },
-
-  "keeps files in order"() {
-    const files = [
-      { path: "/bbb.html", title: "1" },
-      { path: "/ddd.html", title: "2" },
-      { path: "/aaa.html", title: "3" },
-      { path: "/ccc.html", title: "4" },
-    ];
-    const expected = [
-      leaf({ path: "/bbb.html", title: "1" }),
-      leaf({ path: "/ddd.html", title: "2" }),
-      leaf({ path: "/aaa.html", title: "3" }),
-      leaf({ path: "/ccc.html", title: "4" }),
-    ];
-    expect(toc(files), equals, expected);
-  },
-
-  "keeps directories in order"() {
-    const files = [
-      { path: "/index.html", title: "Homepage" },
-      { path: "/aaa/index.html", title: "aaa" },
-      { path: "/aaa/foo.html", title: "foo.html" },
-      { path: "/bbb/index.html", title: "bbb" },
-      { path: "/bbb/foo.html", title: "foo.html" },
-      { path: "/ccc/index.html", title: "ccc" },
-      { path: "/ccc/foo.html", title: "foo.html" },
-      { path: "/ddd/index.html", title: "ddd" },
-      { path: "/ddd/foo.html", title: "foo.html" },
-    ];
-    const expected = [
-      branch(
-        { path: "/aaa/index.html", title: "aaa" },
-        leaf({ path: "/aaa/foo.html", title: "foo.html" })
-      ),
-      branch(
-        { path: "/bbb/index.html", title: "bbb" },
-        leaf({ path: "/bbb/foo.html", title: "foo.html" })
-      ),
-      branch(
-        { path: "/ccc/index.html", title: "ccc" },
-        leaf({ path: "/ccc/foo.html", title: "foo.html" })
-      ),
-      branch(
-        { path: "/ddd/index.html", title: "ddd" },
-        leaf({ path: "/ddd/foo.html", title: "foo.html" })
-      ),
-    ];
-    expect(toc(files), equals, expected);
-  },
-});
-
-test("htmlToc", {
-  "given an empty set of files"() {
-    expect(htmlToc([], "/index.html"), is, "");
-  },
-
-  "given a tree with one file"() {
-    const files = [{ path: "/foo.html", title: "This Is Foo" }];
-
-    const expected = `<ul><li><a href="foo.html">This Is Foo</a></li></ul>`;
-
-    expect(htmlToc(files, "/index.html"), is, expected);
-  },
-
-  "generates a list of multiple links"() {
-    const files = [
-      { path: "/bar.html", title: "Bar" },
-      { path: "/foo.html", title: "Foo" },
-    ];
-
-    const expected = `<ul><li><a href="bar.html">Bar</a></li><li><a href="foo.html">Foo</a></li></ul>`;
-
-    expect(htmlToc(files, "/index.html"), is, expected);
-  },
-
-  "creates relative links, starting from the linkOrigin"() {
-    const files = [{ path: "/foo.html", title: "Foo" }];
-
-    const expected = `<ul><li><a href="../../../foo.html">Foo</a></li></ul>`;
-
-    expect(htmlToc(files, "/one/two/three/foo.html", "/"), is, expected);
-  },
-
-  recurses() {
-    const files = [
-      { path: "/bar/index.html", title: "Bar" },
-      { path: "/bar/baz.html", title: "Baz" },
-    ];
-
-    const expected = `<ul><li><a href="bar/index.html">Bar</a><ul><li><a href="bar/baz.html">Baz</a></li></ul></li></ul>`;
-
-    expect(htmlToc(files, "/index.html"), is, expected);
-  },
-});
+function htmlForToc(toc: TreeOfContents, linkOrigin: string): string {
+  return (
+    "<ul>" +
+    toc
+      .map((node) => {
+        const cssClass =
+          linkOrigin === node.path ? ` class="mdsite-current-file"` : "";
+        const subToc =
+          node.type === "leaf" ? "" : htmlForToc(node.contents, linkOrigin);
+        const relativePath = relative(dirname(linkOrigin), node.path);
+        return `<li${cssClass}><a href="${relativePath}">${node.title}</a>${subToc}</li>`;
+      })
+      .join("") +
+    "</ul>"
+  );
+}
