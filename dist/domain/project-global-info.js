4a855de870dea28dedbd3724626a53ff64167488
Ben Christel <ben.christel@gmail.com>
2024-02-25 08:32:14 -0800

    Move ProjectGlobalInfo tests to their own file

--- dist/domain/project-global-info.js
+++ dist/domain/project-global-info.js
@@ -1,7 +1,4 @@
-import { isAnything } from "../testing/matchers.js";
-import { HtmlFile } from "./html-file.js";
 import { sortHtmlFiles } from "./order.js";
-import { test, expect, equals, which } from "@benchristel/taste";
 export const dummyProjectGlobalInfo = {
     orderedLinkables: [],
     index: {},
@@ -21,73 +18,9 @@ export function ProjectGlobalInfo(files, template) {
     const order = sortHtmlFiles(files);
     return Object.assign(Object.assign({}, indexLinkables(order.map((path) => Linkable(files[path])))), { template });
 }
-test("indexLinkables", {
-    "given an empty array"() {
-        expect(indexLinkables([]), equals, { index: {}, orderedLinkables: [] });
-    },
-    "assigns the first item in the array an index of 0"() {
-        expect(indexLinkables([{ path: "foo" }]), equals, {
-            index: { foo: 0 },
-            orderedLinkables: [{ path: "foo" }],
-        });
-    },
-    "assigns the second item in the array an index of 1"() {
-        expect(indexLinkables([{ path: "foo" }, { path: "bar" }]), equals, {
-            index: { foo: 0, bar: 1 },
-            orderedLinkables: [{ path: "foo" }, { path: "bar" }],
-        });
-    },
-});
 function Linkable(file) {
     return {
         path: file.outputPath,
         title: file.type === "html" ? file.title : "",
     };
 }
-test("ProjectGlobalInfo", {
-    "is empty given no files"() {
-        const files = {};
-        expect(ProjectGlobalInfo(files, ""), equals, {
-            orderedLinkables: [],
-            index: {},
-            template: which(isAnything),
-        });
-    },
-    "given one file"() {
-        const files = {
-            "/a.html": HtmlFile("/a.html", ""),
-        };
-        expect(ProjectGlobalInfo(files, ""), equals, {
-            orderedLinkables: [{ path: "/a.html", title: "a.html" }],
-            index: { "/a.html": 0 },
-            template: which(isAnything),
-        });
-    },
-    "given a template"() {
-        const files = {};
-        expect(ProjectGlobalInfo(files, "the-template").template, equals, "the-template");
-    },
-    "given several files"() {
-        const files = {
-            "/b.html": HtmlFile("/b.html", ""),
-            "/d.html": HtmlFile("/d.html", ""),
-            "/c.html": HtmlFile("/c.html", ""),
-            "/a.html": HtmlFile("/a.html", ""),
-        };
-        expect(ProjectGlobalInfo(files, ""), equals, {
-            orderedLinkables: [
-                { path: "/a.html", title: "a.html" },
-                { path: "/b.html", title: "b.html" },
-                { path: "/c.html", title: "c.html" },
-                { path: "/d.html", title: "d.html" },
-            ],
-            index: {
-                "/a.html": 0,
-                "/b.html": 1,
-                "/c.html": 2,
-                "/d.html": 3,
-            },
-            template: which(isAnything),
-        });
-    },
-});
