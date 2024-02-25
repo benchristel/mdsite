373ce6a36285b6ebb5aa5414e3ad9eba6c737652
Ben Christel <ben.christel@gmail.com>
2024-02-25 08:39:28 -0800

    Move tests for title to their own file

--- dist/domain/title.js
+++ dist/domain/title.js
@@ -1,7 +1,6 @@
 import { basename, dirname } from "path";
 import * as cheerio from "cheerio";
 import { text } from "cheerio";
-import { test, expect, is } from "@benchristel/taste";
 export function title(path, html) {
     const $ = cheerio.load(html);
     return text($("h1").slice(0, 1)) || defaultTitle(path);
@@ -15,29 +14,3 @@ function defaultTitle(path) {
     }
     return basename(path);
 }
-test("title", {
-    "extracts the title from an h1 tag"() {
-        expect(title("", "<h1>foo</h1>"), is, "foo");
-    },
-    "extracts the title from an h1 tag with an id"() {
-        expect(title("", `<h1 id="the-id">foo</h1>`), is, "foo");
-    },
-    "extracts the title from an h1 tag with child tags"() {
-        expect(title("", `<h1><code>foo</code></h1>`), is, "foo");
-    },
-    "uses the title from the first h1 tag if there are several"() {
-        expect(title("", `<h1>the title</h1><h1>not this</h1>`), is, "the title");
-    },
-    "defaults to the filename if there is no h1"() {
-        expect(title("dir/file.html", `<p>whoa</p>`), is, "file.html");
-    },
-    "defaults to the filename if the h1 is empty"() {
-        expect(title("file.html", `<h1></h1>`), is, "file.html");
-    },
-    "defaults to the name of the containing directory if the filename is index.html"() {
-        expect(title("stuff/index.html", ""), is, "stuff");
-    },
-    "defaults to index.html for the root index"() {
-        expect(title("/index.html", ""), is, "index.html");
-    },
-});
---
+++ dist/domain/title.test.js
@@ -0,0 +1,28 @@
+import { test, expect, is } from "@benchristel/taste";
+import { title } from "./title";
+test("title", {
+    "extracts the title from an h1 tag"() {
+        expect(title("", "<h1>foo</h1>"), is, "foo");
+    },
+    "extracts the title from an h1 tag with an id"() {
+        expect(title("", `<h1 id="the-id">foo</h1>`), is, "foo");
+    },
+    "extracts the title from an h1 tag with child tags"() {
+        expect(title("", `<h1><code>foo</code></h1>`), is, "foo");
+    },
+    "uses the title from the first h1 tag if there are several"() {
+        expect(title("", `<h1>the title</h1><h1>not this</h1>`), is, "the title");
+    },
+    "defaults to the filename if there is no h1"() {
+        expect(title("dir/file.html", `<p>whoa</p>`), is, "file.html");
+    },
+    "defaults to the filename if the h1 is empty"() {
+        expect(title("file.html", `<h1></h1>`), is, "file.html");
+    },
+    "defaults to the name of the containing directory if the filename is index.html"() {
+        expect(title("stuff/index.html", ""), is, "stuff");
+    },
+    "defaults to index.html for the root index"() {
+        expect(title("/index.html", ""), is, "index.html");
+    },
+});
