a9dfdedb7f21d9bc95358b4802c97590f78ceb30
Ben Christel <ben.christel@gmail.com>
2024-02-27 20:56:44 -0800

    Create a Project class
    as a step towards fixing rampant feature envy

--- dist/domain/project.js
+++ dist/domain/project.js
@@ -1,21 +1,19 @@
 import { mapEntries } from "../lib/objects.js";
-import { pathAndBufferToProjectFile } from "./files/project-file-set.js";
+import { pathAndBufferToProjectFile, } from "./files/project-file-set.js";
 import { addSyntheticFiles } from "./synthetic-files.js";
 import { ProjectGlobalInfo } from "./project-global-info.js";
 export function buildProject(files, template) {
-    // Step 1: We create "synthetic files" that aren't in the source tree, but
-    // appear in or affect the output. These currently include index.html files,
-    // and in the future might include order.txt files as well.
-    files = addSyntheticFiles(files);
-    // Step 2: Each file figures out what it can about itself using only
-    // information local to that file.
-    const projectFiles = mapEntries(files, pathAndBufferToProjectFile);
-    // Step 3: We synthesize the local information to get global information
-    // about the project: e.g. what order the pages go in.
-    const globalInfo = ProjectGlobalInfo(projectFiles, template);
-    // Step 4: We feed that global information about the project back into each
-    // file, enabling it to render its FINAL FORM!
-    return mapEntries(projectFiles, ([_, projectFile]) => {
-        return projectFile.render(globalInfo);
-    });
+    return new Project(files, template).build();
+}
+class Project {
+    constructor(files, template) {
+        this.files = mapEntries(addSyntheticFiles(files), pathAndBufferToProjectFile);
+        this.template = template;
+    }
+    build() {
+        const globalInfo = ProjectGlobalInfo(this.files, this.template);
+        return mapEntries(this.files, ([_, projectFile]) => {
+            return projectFile.render(globalInfo);
+        });
+    }
 }
--- src/domain/project.ts
+++ src/domain/project.ts
@@ -1,26 +1,33 @@
 import { FileSet } from "../lib/files.js";
 import { mapEntries } from "../lib/objects.js";
-import { pathAndBufferToProjectFile } from "./files/project-file-set.js";
+import {
+  ProjectFileSet,
+  pathAndBufferToProjectFile,
+} from "./files/project-file-set.js";
 import { addSyntheticFiles } from "./synthetic-files.js";
 import { ProjectGlobalInfo } from "./project-global-info.js";
 
 export function buildProject(files: FileSet, template: string): FileSet {
-  // Step 1: We create "synthetic files" that aren't in the source tree, but
-  // appear in or affect the output. These currently include index.html files,
-  // and in the future might include order.txt files as well.
-  files = addSyntheticFiles(files);
+  return new Project(files, template).build();
+}
 
-  // Step 2: Each file figures out what it can about itself using only
-  // information local to that file.
-  const projectFiles = mapEntries(files, pathAndBufferToProjectFile);
+class Project {
+  files: ProjectFileSet;
+  template: string;
 
-  // Step 3: We synthesize the local information to get global information
-  // about the project: e.g. what order the pages go in.
-  const globalInfo = ProjectGlobalInfo(projectFiles, template);
+  constructor(files: FileSet, template: string) {
+    this.files = mapEntries(
+      addSyntheticFiles(files),
+      pathAndBufferToProjectFile
+    );
+    this.template = template;
+  }
 
-  // Step 4: We feed that global information about the project back into each
-  // file, enabling it to render its FINAL FORM!
-  return mapEntries(projectFiles, ([_, projectFile]) => {
-    return projectFile.render(globalInfo);
-  });
+  build() {
+    const globalInfo = ProjectGlobalInfo(this.files, this.template);
+
+    return mapEntries(this.files, ([_, projectFile]) => {
+      return projectFile.render(globalInfo);
+    });
+  }
 }
